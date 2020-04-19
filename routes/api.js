0b648623cd16d47a91d080ebf16b4044ba50d25e
Adam Siemer <14990007+abcmer@users.noreply.github.com>
2020-04-19 08:13:45 -0500

    update post games function to generate empty seats

--- routes/api.js
+++ routes/api.js
@@ -47,7 +47,29 @@ router.get('/games/:id', (req, res, next) => {
     .catch(next)
 })
 
-router.post('/games', (req, res, next) => {
+router.post('/games', async (req, res, next) => {
+
+  const maxPlayers = req.body.config.maxPlayers
+  const initialChipCount = req.body.config.initialChipCount
+
+  console.log(maxPlayers)
+  console.log('req.body', req.body)
+
+
+  let seats = []
+  for (i=0; i < maxPlayers; i++) {
+    seats.push({
+      "seatNumber": i,
+      "userId": null,
+      "chipCount": initialChipCount,
+    })
+  }
+
+  req.body.seats = seats
+  console.log('seats', seats[0])
+
+
+
   Game.create(req.body)
   .then(data => res.json(data))
   .catch(next)
@@ -57,6 +79,16 @@ router.delete('/games/:id', (req, res, next) => {
   Game.findOneAndDelete({"_id": req.params.id})
     .then(data => res.json(data))
     .catch(next)
+})
+
+router.delete('/games', (req, res, next) => {
+  Game.deleteMany({}, function(err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  })
 })
 
 router.put('/games/:id', async (req, res, next) => {
